05c77362f24669429d1447bf2c44c2827821a5b5
esok <esok0617@gmail.com>
2025-12-28 01:45:07 +0900

    feat: testing example & setting

---
+++ tests/test_hello.py
@@ -0,0 +1,13 @@
+# 테스트-hello
+def test_hello():
+    assert True
+    
+def test_hello():
+    assert False
+
+# 더하기 함수 테스트하기
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
